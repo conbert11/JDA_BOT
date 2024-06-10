cc6bd4982208c73d07134608c6a3b8db9f4b1ce9
conbert11 <c.berthun@web.de>
2024-06-10 20:20:09 +0200

    Backup-1

---
+++ cbbot/src/main/java/org/example/secrets.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class secrets extends ListenerAdapter {
+
+    public static String TOKEN = "MTI0OTMxNjg2NjA1MzI0NzA1Nw.GJj0oo.T5K-OIm0Wts2Mgz0dzmSHoUDuoeL1L6QMvXh3Y";
+}
