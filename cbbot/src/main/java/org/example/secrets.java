90d85157c986ebf738dcb3621c2b5261714dba5d
conbert11 <155236656+conbert11@users.noreply.github.com>
2025-02-09 09:14:18 +0100

    Update secrets.java

--- cbbot/src/main/java/org/example/secrets.java
+++ cbbot/src/main/java/org/example/secrets.java
@@ -4,5 +4,5 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class secrets extends ListenerAdapter {
 
-    public static String TOKEN = "MTI0OTMxNjg2NjA1MzI0NzA1Nw.GJj0oo.T5K-OIm0Wts2Mgz0dzmSHoUDuoeL1L6QMvXh3Y";
+    public static String TOKEN = "YOUR_DISCORD_BOT_TOKEN";
 }
