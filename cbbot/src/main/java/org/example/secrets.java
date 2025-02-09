c9ccdb46b16a3fc08452fee91276328d607c7cfe
conbert11 <155236656+conbert11@users.noreply.github.com>
2025-02-09 09:17:00 +0100

    Delete cbbot directory

--- cbbot/src/main/java/org/example/secrets.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
-public class secrets extends ListenerAdapter {
-
-    public static String TOKEN = "YOUR_DISCORD_BOT_TOKEN";
-}
